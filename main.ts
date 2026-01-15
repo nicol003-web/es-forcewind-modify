50e31dbdd35e9bd9f693c1ba24b45551c9c30842
nicol003-web <nicoldaniela003@gmail.com>
2026-01-14 20:01:58 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 fwdSensors.touch.fwdOnTouch(jacdac.ButtonEvent.Down, function () {
-    fwdMotors.middleServo.fwdSetSpeed(50)
+    fwdMotors.middleServo.fwdSetSpeed(0)
 })
 fwdSensors.dial1.fwdOnDialTurned(fwdSensors.DialDirection.CCW, function (difference) {
-    fwdMotors.middleServo.fwdSetSpeed(50)
+    fwdMotors.middleServo.fwdSetSpeed(-50)
 })
 fwdSensors.dial1.fwdOnDialTurned(fwdSensors.DialDirection.CW, function (difference) {
-    fwdMotors.middleServo.fwdSetSpeed(50)
+    fwdMotors.middleServo.fwdSetSpeed(-50)
 })
