ce3ab2efb3ecca573c3b5eaf748abc61d88f51b3
nicol003-web <nicoldaniela003@gmail.com>
2026-01-14 20:00:33 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+fwdSensors.touch.fwdOnTouch(jacdac.ButtonEvent.Down, function () {
+    fwdMotors.middleServo.fwdSetSpeed(50)
+})
+fwdSensors.dial1.fwdOnDialTurned(fwdSensors.DialDirection.CCW, function (difference) {
+    fwdMotors.middleServo.fwdSetSpeed(50)
+})
+fwdSensors.dial1.fwdOnDialTurned(fwdSensors.DialDirection.CW, function (difference) {
+    fwdMotors.middleServo.fwdSetSpeed(50)
+})
